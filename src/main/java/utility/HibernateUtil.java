a33b16ffa0dd68f1e53ba2b1e654ea360cb4d497
T490 <vunguyenhariven@gmail.com>
2023-03-13 14:46:04 +0700

    Init project

---
+++ src/main/java/utility/HibernateUtil.java
@@ -0,0 +1,37 @@
+package utility;
+
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+    
+    private static final SessionFactory FACTORY;
+
+    static {
+        Configuration conf = new Configuration();
+
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=FINALASS_FPOLYSHOP_FA22_SOF205__SOF2041");
+        properties.put(Environment.USER, "test");
+        properties.put(Environment.PASS, "qwer");
+
+        conf.setProperties(properties);
+//        conf.addAnnotatedClass(ChucVu.class);
+
+        ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(conf.getProperties()).build();
+        FACTORY = conf.buildSessionFactory(registry);
+
+    }
+
+    public static SessionFactory getFACTORY() {
+        return FACTORY;
+    }
+
+}
