4a649de662a9b88727df73737a1c2d44d387d348
T490 <vunguyenhariven@gmail.com>
2023-03-20 10:07:14 +0700

    ha commit 1006-200323

--- src/main/java/Models/MauSac.java
+++ src/main/java/Models/MauSac.java
@@ -1,5 +1,33 @@
 package Models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "MauSac")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class MauSac {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "MaMauSac")
+    private String maMauSac;
+
+    @Column(name = "TenMauSac")
+    private String tenMauSac;
+
 }
