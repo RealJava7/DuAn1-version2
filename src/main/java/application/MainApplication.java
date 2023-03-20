7ad5699ebfe4db7d5a2d57ac8e7699612bb44436
T490 <vunguyenhariven@gmail.com>
2023-03-20 21:39:00 +0700

    ha commit 2138-20032023

---
+++ src/main/java/application/MainApplication.java
@@ -0,0 +1,10 @@
+package application;
+
+import view.PhanMemBanDienThoai;
+
+public class MainApplication {
+    
+    public static void main(String[] args) {
+        new PhanMemBanDienThoai().setVisible(true);
+    }
+}
