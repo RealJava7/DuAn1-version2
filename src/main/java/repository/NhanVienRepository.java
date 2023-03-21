654a5558618e7e9ba5fb9754ed7806ce73be99e3
T490 <vunguyenhariven@gmail.com>
2023-03-21 10:49:16 +0700

    ha commit 1049-210323

--- src/main/java/repository/NhanVienRepository.java
+++ src/main/java/repository/NhanVienRepository.java
@@ -1,5 +1,6 @@
 package repository;
 
+import java.time.LocalDate;
 import model.NhanVien;
 import model.TaiKhoan;
 import org.hibernate.HibernateException;
@@ -10,7 +11,7 @@ import utility.HibernateUtil;
 public class NhanVienRepository {
     
     // 1. add
-    public boolean add(NhanVien nhanVien, TaiKhoan taiKhoan) {
+    public static boolean add(NhanVien nhanVien) {
         boolean check = false;
         try {
             Session session = HibernateUtil.getFACTORY().openSession();
@@ -22,5 +23,27 @@ public class NhanVienRepository {
             ex.printStackTrace(System.out);
         }
         return check;
+    }
+    
+    public static void main(String[] args) {
+        NhanVien nhanVien = new NhanVien();
+        
+        nhanVien.setHoTen("Nguyễn Khắc Thịnh");
+        nhanVien.setGioiTinh(true);
+        nhanVien.setSdt("091232829112");
+        nhanVien.setNgaySinh(LocalDate.now());
+        nhanVien.setDiaChi("262 Phạm Văn Đồng");
+        nhanVien.setEmail("thinh12@gmail.com");
+        nhanVien.setChucVu(true);
+        nhanVien.setTrangThai(false);
+        nhanVien.setHinhAnh("");
+        
+        TaiKhoan taiKhoan = new TaiKhoan();
+        taiKhoan.setTaiKhoan("thingnguyen123");
+        taiKhoan.setMatKhau("12312092");
+        
+        nhanVien.setTaiKhoan(taiKhoan);
+        
+        System.out.println(add(nhanVien));
     }
 }
--- src/main/java/utility/HibernateUtil.java
+++ src/main/java/utility/HibernateUtil.java
@@ -1,10 +1,18 @@
 package utility;
 
 import java.util.Properties;
+import model.CameraChiTiet;
 import model.DienThoai;
+import model.DongSanPham;
+import model.Hang;
+import model.Imei;
 import model.KhachHang;
+import model.ManHinhChiTiet;
+import model.MauSac;
 import model.NhanVien;
 import model.PhieuGiamGia;
+import model.TaiKhoan;
+import model.TheTichDiem;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -21,15 +29,23 @@ public class HibernateUtil {
         Properties properties = new Properties();
         properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
         properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=duan1");
-        properties.put(Environment.USER, "sa");
-        properties.put(Environment.PASS, "2");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=duan1_spring2023");
+        properties.put(Environment.USER, "test");
+        properties.put(Environment.PASS, "qwer");
 
         conf.setProperties(properties);
         conf.addAnnotatedClass(PhieuGiamGia.class);
         conf.addAnnotatedClass(DienThoai.class);
+        conf.addAnnotatedClass(CameraChiTiet.class);
+        conf.addAnnotatedClass(ManHinhChiTiet.class);
+        conf.addAnnotatedClass(DongSanPham.class);
+        conf.addAnnotatedClass(Imei.class);
+        conf.addAnnotatedClass(MauSac.class);
+        conf.addAnnotatedClass(Hang.class);
         conf.addAnnotatedClass(KhachHang.class);
+        conf.addAnnotatedClass(TheTichDiem.class);
         conf.addAnnotatedClass(NhanVien.class);
+        conf.addAnnotatedClass(TaiKhoan.class);
 
         ServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .applySettings(conf.getProperties()).build();
