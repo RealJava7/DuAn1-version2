a9d7994f39d99c98b324327df4db6edc114364c8
Pham Van Binh <binhpvph29510>
2023-03-21 21:50:17 +0700

    bình commit service impl

--- src/main/java/service/QuanLyKhachHangService.java
+++ src/main/java/service/QuanLyKhachHangService.java
@@ -1,5 +1,10 @@
 package service;
 
+import java.util.List;
+import viewmodel.KhachHangResponse;
+
 public interface QuanLyKhachHangService {
+
+    List<KhachHangResponse> getAll();
 
 }
--- src/main/java/service/impl/QuanLyKhachHangServiceImpl.java
+++ src/main/java/service/impl/QuanLyKhachHangServiceImpl.java
@@ -1,7 +1,18 @@
 package service.impl;
 
+import java.util.List;
+import model.KhachHang;
+import repository.KhachHangRepository;
 import service.QuanLyKhachHangService;
+import viewmodel.KhachHangResponse;
 
 public class QuanLyKhachHangServiceImpl implements QuanLyKhachHangService {
+
+    KhachHangRepository repo = new KhachHangRepository();
+
+    @Override
+    public List<KhachHangResponse> getAll() {
+        List<KhachHang> lists = repo.
+    }
 
 }
