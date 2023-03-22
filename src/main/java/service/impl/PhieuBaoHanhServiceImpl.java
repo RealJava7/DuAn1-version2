33eaf16eb0dbbb49d134ed8671c398fb1890e8e6
hieupvph29564 <120153647+hieupvph29564@users.noreply.github.com>
2023-03-22 10:58:28 +0700

    Hiếu - Thêm phieubaohanh, ctphieubaohanh repository và code service.impl

---
+++ src/main/java/service/impl/PhieuBaoHanhServiceImpl.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service.impl;
+
+import java.util.List;
+import service.PhieuBaoHanhService;
+import viewmodel.PhieuBaoHanhReponse;
+
+/**
+ *
+ * @author virus
+ */
+public class PhieuBaoHanhServiceImpl implements PhieuBaoHanhService {
+
+    @Override
+    public List<PhieuBaoHanhReponse> getAllPhieuBaoHanh() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
