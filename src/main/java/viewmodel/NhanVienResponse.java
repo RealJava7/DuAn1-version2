448744f5bea36ba3d0f1eb1405f9ecab3a6e5d98
thuymtph29502 <thuymtph29502@fpt.edu.vn>
2023-03-23 13:46:27 +0700

    Thuy commit 1346 230323 thêm cons class nv, tk, nvres

--- src/main/java/viewmodel/NhanVienResponse.java
+++ src/main/java/viewmodel/NhanVienResponse.java
@@ -29,6 +29,20 @@ public class NhanVienResponse {
     public String toString() {
         return "NhanVienResponse{" + "id=" + id + ", hoTen=" + hoTen + ", gioiTinh=" + gioiTinh + ", sdt=" + sdt + ", ngaySinh=" + ngaySinh + ", diaChi=" + diaChi + ", email=" + email + ", chucVu=" + chucVu + ", trangThai=" + trangThai + ", hinhAnh=" + hinhAnh + ", taiKhoan=" + taiKhoan + ", matKhau=" + matKhau + '}';
     }
+
+    public NhanVienResponse(String hoTen, boolean gioiTinh, String sdt, LocalDate ngaySinh, String diaChi, String email, boolean chucVu, boolean trangThai, String hinhAnh, String taiKhoan, String matKhau) {
+        this.hoTen = hoTen;
+        this.gioiTinh = gioiTinh;
+        this.sdt = sdt;
+        this.ngaySinh = ngaySinh;
+        this.diaChi = diaChi;
+        this.email = email;
+        this.chucVu = chucVu;
+        this.trangThai = trangThai;
+        this.hinhAnh = hinhAnh;
+        this.taiKhoan = taiKhoan;
+        this.matKhau = matKhau;
+    }
     
     
 }
